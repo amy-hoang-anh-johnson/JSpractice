7b262523424d77a503843575b6a6b6d7c26f51c1
Amy Hoang Anh Johnson <72709428+up893788@users.noreply.github.com>
2022-12-30 20:11:48 +0000

    2ndPractice

---
+++ 2ndPractice.js
@@ -0,0 +1,23 @@
+//my age
+var myAge = 24;
+
+//value saved to this var will change
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+//myAge - 2
+let laterYears = myAge - 2;
+
+//Dog years
+laterYears *= 4;
+
+console.log(earlyYears+' '+laterYears);
+
+//adding earlyYears and laterYears
+var myAgeInDogYears = earlyYears + laterYears;
+
+//my name
+var myName = 'Amy'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
