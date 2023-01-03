6ff7341b8f8caac0edc009c9d37c02038a926ab6
Amy Hoang Anh Johnson <72709428+up893788@users.noreply.github.com>
2023-01-03 14:24:53 +0000

    Create 7thPractice.js

---
+++ 7thPractice.js
@@ -0,0 +1,41 @@
+var userName = "Amy";
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+const userQuestion = "How are you?";
+
+console.log(`${userName}, ${userQuestion}`);
+
+randomNumber = Math.floor(Math.random() * 7);
+
+let eightball = '';
+
+switch(randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+console.log(`Magic Eight Ball says, ${eightBall}.`);
+
