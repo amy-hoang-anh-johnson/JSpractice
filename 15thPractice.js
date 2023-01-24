ab3d0b0b6bd3a0172fb785cf808b5674fd6fdf3b
Amy Hoang Anh Johnson <72709428+up893788@users.noreply.github.com>
2023-01-24 14:48:00 +0000

    Create 15thPractice.js

---
+++ 15thPractice.js
@@ -0,0 +1,24 @@
+let input = 'turpentine and turtles';
+let vowels = ['a','e','i','o','u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === 'e') {
+    resultArray.push(input[i]);
+  }
+  if (input[i] === 'u') {
+    resultArray.push(input[i]);
+  } 
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+      console.log(input[i]);
+    }
+  }
+}
+
+console.log(resultArray);
+
+let resultString = resultArray.join('').toUpperCase();
+
+console.log(resultString);
